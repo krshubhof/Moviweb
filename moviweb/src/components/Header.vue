b77d15b8f9cfce92678f3a92efda4ce51ad758b3
krshubhof <shubhamsinha086@gmail.com>
2018-06-12 22:15:49 +0530

    Navbar done

--- moviweb/src/components/Header.vue
+++ moviweb/src/components/Header.vue
@@ -1,6 +1,17 @@
 <template>
-  <v-app>
-    <v-toolbar></v-toolbar>
+   <v-app >
+    <v-toolbar class="blue" hide-overlay>
+      <v-toolbar-title>MW</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat>World</v-btn>
+        <v-btn flat>India</v-btn>
+        <v-btn flat>Movie</v-btn>
+        <v-btn flat>Politics</v-btn>
+        <v-btn flat>Sports</v-btn>
+        <v-btn flat>Business</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
   </v-app>
 </template>
 
@@ -8,7 +19,7 @@
 export default {
   data() {
       return {
-       
+         sideNav: false
   }
 }
 }
